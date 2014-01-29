b808ee074aa4302e364e1dc8b3f735b7d32e21d9
sean@seanbdurkin.id.au <sean@seanbdurkin.id.au@5055a572-2d42-0a9d-8178-233373f86929>
2014-01-29 08:17:12 +0000

    Tasks 4 through to to 8

--- the-demo/run/Demo.TaskFive.pas
+++ the-demo/run/Demo.TaskFive.pas
@@ -4,8 +4,10 @@ uses Demo.Task, SBD.ServiceProvider, SysUtils;
 
 type
 [Name('Service discrimination by cookie')]
-[Preface('Preface - to be developed.')]
-[ToBeDeveloped]
+[Preface('When we register a service we get an integer token for each service'#13#10 +
+         'This token can then be used to discriminate between multiple competing'#13#10 +
+         'implementations of a service using the ActiveArrayMemberCookie property.'#13#10 +
+         'Take a look at unit Demo.TaskFive for example.')]
 TTaskFive = class sealed( TTask)
   protected
     procedure Execute( Put: TLogLineProc);       override;
@@ -15,18 +17,79 @@ implementation
 
 
 
+type
+IPerson = interface
+  ['{348F5700-660C-4495-93F7-65D0DF902F31}']
+    function Name: string;
+  end;
+
+TPerson = class abstract( TInterfacedObject, IPerson)
+  protected
+    function Name: string;       virtual; abstract;
+  public
+    [configuration] constructor CreatePerson;
+  end;
+
+TSean = class sealed( TPerson)
+  protected
+    function Name: string;       override;
+  end;
+
+TChris = class sealed( TPerson)
+  protected
+    function Name: string;       override;
+  end;
+
 procedure TTaskFive.Execute( Put: TLogLineProc);
 var
   Provider: IServiceProvider;
+  Person: IPerson;
+ {$if CompilerVersion < 22}
+    TokenForChris: integer;
+  {$else}
+    TokenForChris: TIntegers;
+  {$ifend}
+
 begin
 Provider := StandardServiceProvider;
 // Note: In most applications, you will follow the good practice of
 //  segregating concrete class registration with service resolution.
 //  Here it is together, only for demonstration purposes.
+Provider.RegisterServiceClass( IPerson, TSean);
+TokenForChris := Provider.RegisterServiceClass( IPerson, TChris);
+// In Delphi 2010, RegisterServiceClass returns an integer token for the first service registered.
+// In late compilers, it returns an array of integer tokens, one for each service registered.
 
+Provider.ActiveArrayMemberCookie[ IPerson, ''] :=
+ {$if CompilerVersion < 22}
+    TokenForChris;
+  {$else}
+    TokenForChris[0];
+  {$ifend}
+ // ^ Having remembered the cookie, this is how we can discriminate between multiple solutions
+ //    by cookie.
+if Provider.Gn.Acquire<IPerson>( nil, Person) then
+  Put( Format( 'Of Sean and Chris, %s was selected', [Person.Name]));
 Provider.ShutDown
 end;
 
 
+
+
+constructor TPerson.CreatePerson;
+begin
+end;
+
+
+function TSean.Name: string;
+begin
+result := 'Sean'
+end;
+
+
+function TChris.Name: string;
+begin
+result := 'Chris'
+end;
 
 end.
--- the-demo/run/Demo.TaskSix.pas
+++ the-demo/run/Demo.TaskSix.pas
@@ -4,8 +4,11 @@ uses Demo.Task, SBD.ServiceProvider, SysUtils;
 
 type
 [Name('Service discrimination by name')]
-[Preface('Preface - to be developed.')]
-[ToBeDeveloped]
+[Preface('When we register a service we specify a string id for this particular solution.'#13#10 +
+         'using the [configuration] attribute with parameter in format ''Id=Something'''#13#10 +
+         'This id can then be used to discriminate between multiple competing'#13#10 +
+         'implementations of a service using the ActiveArrayMemberName property.'#13#10 +
+         'Take a look at unit Demo.TaskSix for example.')]
 TTaskSix = class sealed( TTask)
   protected
     procedure Execute( Put: TLogLineProc);       override;
@@ -14,20 +17,70 @@ TTaskSix = class sealed( TTask)
 implementation
 
 
+type
+IPerson = interface
+  ['{348F5700-660C-4495-93F7-65D0DF902F31}']
+    function Name: string;
+  end;
+
+TPerson = class abstract( TInterfacedObject, IPerson)
+  protected
+    function Name: string;       virtual; abstract;
+  end;
+
+TSean = class sealed( TPerson)
+  protected
+    function Name: string;       override;
+  public
+    [configuration] constructor CreatePerson;
+  end;
+
+TChris = class sealed( TPerson)
+  protected
+    function Name: string;       override;
+  public
+    [configuration('Id=ThisOne')] constructor CreatePerson;
+  end;
+
 
 procedure TTaskSix.Execute( Put: TLogLineProc);
 var
   Provider: IServiceProvider;
+  Person: IPerson;
 begin
 Provider := StandardServiceProvider;
 // Note: In most applications, you will follow the good practice of
 //  segregating concrete class registration with service resolution.
 //  Here it is together, only for demonstration purposes.
+Provider.RegisterServiceClass( IPerson, TSean);
+Provider.RegisterServiceClass( IPerson, TChris);
+// In Delphi 2010, RegisterServiceClass returns an integer token for the first service registered.
+// In late compilers, it returns an array of integer tokens, one for each service registered.
 
+Provider.ActiveArrayMemberName[ IPerson, ''] := 'ThisOne';
+if Provider.Gn.Acquire<IPerson>( nil, Person) then
+  Put( Format( 'Of Sean and Chris, %s was selected', [Person.Name]));
 Provider.ShutDown
 end;
 
 
+constructor TSean.CreatePerson;
+begin
+end;
 
+function TSean.Name: string;
+begin
+result := 'Sean'
+end;
+
+
+constructor TChris.CreatePerson;
+begin
+end;
+
+function TChris.Name: string;
+begin
+result := 'Chris'
+end;
 end.
 
