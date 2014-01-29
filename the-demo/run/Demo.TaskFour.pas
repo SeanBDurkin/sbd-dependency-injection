e290ead228a25a75c1a77161111885addcf76e6b
sean@seanbdurkin.id.au <sean@seanbdurkin.id.au@5055a572-2d42-0a9d-8178-233373f86929>
2014-01-29 06:35:04 +0000

    Task 4 implemented

--- the-demo/run/Demo.TaskFour.pas
+++ the-demo/run/Demo.TaskFour.pas
@@ -5,8 +5,11 @@ uses Demo.Task, SBD.ServiceProvider, SysUtils;
 type
 
 [Name('Same class, multiple configurations of service')]
-[Preface('Preface - to be developed.')]
-[ToBeDeveloped]
+[Preface('It is easy to register a service class that services multiple configurations.'#13#10 +
+         'Use the [configuration] attribute on a constructor with one string parameter'#13#10 +
+         'The [configuration] attribute takes as a parameter a comma-seperated'#13#10 +
+         'list of config strings.'#13#10 +
+         'Take a look at unit Demo.TaskFour for example.')]
 TTaskFour = class sealed( TTask)
   protected
     procedure Execute( Put: TLogLineProc);       override;
@@ -14,20 +17,48 @@ TTaskFour = class sealed( TTask)
 
 implementation
 
+type
+INamedThing = interface
+  ['{F883ED5E-45CA-4446-B076-C9B9C840D336}']
+    function Name: string;
+  end;
+
+TPerson = class( TInterfacedObject, INamedThing)
+  private
+    FName: string;
+    function Name: string;
+  public
+    [configuration('Sean,Chris')] constructor CreatePerson( const Config: string);
+  end;
 
 
 procedure TTaskFour.Execute( Put: TLogLineProc);
 var
   Provider: IServiceProvider;
+  Person: INamedThing;
 begin
 Provider := StandardServiceProvider;
 // Note: In most applications, you will follow the good practice of
 //  segregating concrete class registration with service resolution.
 //  Here it is together, only for demonstration purposes.
-
+Provider.RegisterServiceClass( INamedThing, TPerson);
+if Provider.Gn.Acquire<INamedThing>( nil, Person, 'Chris') then
+  Put( Format( 'There were two people registered. The Chris service said his name was %s.', [Person.Name]));
 Provider.ShutDown
 end;
 
 
+
+{ TPerson }
+
+constructor TPerson.CreatePerson(const Config: string);
+begin
+FName := Config
+end;
+
+function TPerson.Name: string;
+begin
+result := FName
+end;
 
 end.
